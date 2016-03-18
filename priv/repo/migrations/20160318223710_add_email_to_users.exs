1472b6c0b54102d13c4c794d4c98f7f50cd7273c
Paulo Ancheta <paulo.ancheta@gmail.com>
2016-03-18 15:45:08 -0700

    add an email to users

---
+++ priv/repo/migrations/20160318223710_add_email_to_users.exs
@@ -0,0 +1,9 @@
+defmodule HerokuTestApp.Repo.Migrations.AddEmailToUsers do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      add :email, :string
+    end
+  end
+end
--- web/models/user.ex
+++ web/models/user.ex
@@ -2,14 +2,15 @@ defmodule HerokuTestApp.User do
   use HerokuTestApp.Web, :model
 
   schema "users" do
-    field :name, :string
-    field :age, :integer
+    field :name,  :string
+    field :age,   :integer
+    field :email, :string
 
     timestamps
   end
 
-  @required_fields ~w(name age)
-  @optional_fields ~w()
+  @required_fields ~w(name)
+  @optional_fields ~w(age email)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
